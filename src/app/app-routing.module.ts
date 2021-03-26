9a8480059c23a1dd22c737e8a9aa4ef69b3013d2
nicolas aramayo <aramayo@gmail.com>
2021-03-25 22:05:37 -0300

    primer commit para heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'quiensoy', component: QuienSoyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
