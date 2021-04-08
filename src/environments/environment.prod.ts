250e1aec774b595a45f9f1693a4e260290c5cd94
nicolas aramayo <aramamayo.fabian@gmail.com>
2021-04-08 08:31:41 -0300

    Arreglo para prod ;)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  
+  firebaseConfig : {
+    apiKey: "AIzaSyC7YN4czCugNfGIdJt7ZL9b2WGV-9M0OEk",
+    authDomain: "saladejuegos-2a3a7.firebaseapp.com",
+    projectId: "saladejuegos-2a3a7",
+    storageBucket: "saladejuegos-2a3a7.appspot.com",
+    messagingSenderId: "635336417860",
+    appId: "1:635336417860:web:f38722a2d0f271b2381211",
+    measurementId: "G-4W831RY8WG"
+  }
 };
